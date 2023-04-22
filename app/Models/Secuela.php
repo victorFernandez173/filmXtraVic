5a90633cc022ae16a2abab6fa1ee5c5586e9aa63
victor.fernandez173 <victor.fer.gor@proton.me>
2023-04-22 18:09:44 +0200

    Finalizado: modelo Secuela.php y su migración. Corregidos errores de código.

---
+++ app/Models/Secuela.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Secuela extends Model
+{
+    const CREATED_AT = 'creada';
+    const UPDATED_AT = 'modificada';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'orden',
+    ];
+}
---
+++ database/migrations/2023_04_22_160022_create_secuelas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Secuela;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('secuelas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('obra_id')->constrained();
+            $table->foreignId('saga_id')->constrained();
+            $table->integer('orden')->unsigned();
+
+            $table->unique(['obra_id', 'saga_id', 'orden']);
+
+            $table->timestamp(Secuela::CREATED_AT);
+            $table->timestamp(Secuela::UPDATED_AT);
+
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('secuelas');
+    }
+};
