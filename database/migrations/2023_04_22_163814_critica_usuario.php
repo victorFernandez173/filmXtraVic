7f1a1b38c052bd733c4ef2f677f798abc0c94ff3
victor.fernandez173 <victor.fer.gor@proton.me>
2023-04-22 18:50:41 +0200

    Añadida migración tabla intermedia para likes critica-usuario

---
+++ database/migrations/2023_04_22_163814_critica_usuario.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->foreignId('critica_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+
+            $table->unique(['critica_id', 'user_id']);
+
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
