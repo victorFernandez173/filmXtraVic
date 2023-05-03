ea98bb5061bdba47bcff6fc6feee8b5e57c036cb
victorFernandez173 <victor.fer.gor@proton.me>
2023-05-03 21:52:38 +0200

    commit test2

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -9,7 +9,7 @@ const navigation = [
     {name: 'Trailers', href: '#', current: false},
     {name: 'Premiadas', href: '#', current: false},
 ]
-//hola
+//holaflskjfjflakjdkfjdasklfjaklfjsdfsajfskljfdkdggdafsdfdsfffjasldfsdlfdl
 </script>
 
 <template>
