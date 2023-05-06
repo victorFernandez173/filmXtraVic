bc9b6bbb28070c1d41899c3b9ed8bd50696fd40a
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-06 19:45:11 +0200

    Refactorizaciones de comentarios.

--- resources/js/Layouts/LayoutPrincipal.vue
+++ resources/js/Layouts/LayoutPrincipal.vue
@@ -70,7 +70,6 @@ import { Link } from "@inertiajs/vue3";
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
                 <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li class="activo">
-<!--TODO  ->  HE SUSTITUIDO TODOS LOS <a> por <Link> YA QUE CON INERTIA ESTO ES NECESARIO POR LO VISTO PARA MANTERNER FUNCIONALIDADES DE VUE, EN LOS ESTILOS CREO QUE SE PUEDE DEJAR a{...} COMO SELECTOR Y SE SIGUEN APLICANDO BIEN LOS ESTILOS   -->
                         <Link :href="route('/')" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent color md:p-0">Inicio</Link>
                     </li>
                     <li>
