5b12d6ccf0a7f81d734b5e8ac4293a56ba777962
elena <elena.gatajimenez@gmail.com>
2023-05-11 17:59:53 +0200

    Formulario Filtrado Elena modificado

--- resources/js/Components/FormularioFiltrado.vue
+++ resources/js/Components/FormularioFiltrado.vue
@@ -28,37 +28,35 @@ defineProps(['generos', 'paises']);
     <form @submit.prevent="submit" class="m-auto">
 
         <!-- Div desplegables y boton aplicar -->
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-7 text-center w-full justify-items-center filtrar-3">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-9 text-center w-full justify-items-center filtrar-3">
             <!-- Select genero -->
             <InputLabel for="idGenero" value="Genero"/>
-            <select name="genero" v-model="form.genero" id="idGenero" class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
+            <select name="genero" v-model="form.genero" id="idGenero" class="bg-gray-50 border border-gray-300 text-gray-900 text-base focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
                 <option disabled value="">-</option>
                 <option v-for="genero in generos">{{ genero['genero']}}</option>
             </select>
 
             <!-- Select pais -->
             <InputLabel for="idPais" value="Pais"/>
-            <select name="pais" id="idPais" v-model="form.pais" class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
+            <select name="pais" id="idPais" v-model="form.pais" class="bg-gray-50 border border-gray-300 text-gray-900 text-base focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
                 <option disabled value="">-</option>
                 <option v-for="pais in paises">{{ pais['pais']}}</option>
             </select>
 
             <!-- Select periodo -->
             <InputLabel for="idDesde" value="Desde"/>
-            <select name="desde" id="idDesde" v-model="form.desde" class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
+            <select name="desde" id="idDesde" v-model="form.desde" class="bg-gray-50 border border-gray-300 text-gray-900 text-base focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
                 <option disabled value="">-</option>
                 <option v-for="n in 154">{{ n + 1869 }}</option>
             </select>
             <InputLabel for="idHasta" value="Hasta"/>
-            <select name="hasta" id="idHasta" v-model="form.hasta" class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
-                <option disabled value="-DDDDD">-</option>
+            <select name="hasta" id="idHasta" v-model="form.hasta" class="bg-gray-50 border border-gray-300 text-gray-900 text-base focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
+                <option disabled value="">-</option>
                 <option v-for="n in 154">{{ n + 1869 }}</option>
             </select>
 
-
-
             <!-- Boton aplicar -->
-            <PrimaryButton type="button" class="boton-filtrar text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-5" :disabled="form.processing" :class="{ 'opacity-25': form.processing }">FILTRAR</PrimaryButton>
+            <PrimaryButton type="button" class="boton-filtrar text-white font-medium text-sm px-5 py-2.5 mr-2 mb-2 mt-5" :disabled="form.processing" :class="{ 'opacity-25': form.processing }">FILTRAR</PrimaryButton>
         </div>
     </form>
 </template>
@@ -69,9 +67,4 @@ form {
     padding: 20px;
     border: #e37f81 3px solid;
 }
-
-
-/*******Seccion desplegables*******/
-
-
 </style>
