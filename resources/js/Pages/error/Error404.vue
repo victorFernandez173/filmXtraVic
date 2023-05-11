684998202a1821315cb1af7ddb8a8c7625503452
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-11 09:24:52 +0200

    Refactorizacion en pagina  Error404.vue

--- resources/js/Pages/error/Error404.vue
+++ resources/js/Pages/error/Error404.vue
@@ -1,15 +1,21 @@
 <script setup>
+import { Head } from "@inertiajs/vue3";
+
 defineProps([ 'status', 'message'])
 </script>
 
 <template>
+    <Head>
+        <title>Error 404</title>
+        <meta name="filter" content="Página de error 404">
+    </Head>
     <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div class="text-center">
             <p class="texto-404 text-base font-semibold">Error {{ status }}</p>
             <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{{ message }}</h1>
             <p class="mt-6 text-base leading-7 text-gray-600">Pero si vuelves a la home, en FilmXtra encontrarás todo lo que necesitas.</p>
 
-<!--<img src="/images/logo.png" alt="LO QUE TE DE LA REAL GANA">-->
+<!--Ya sabes el status y/o message lo puedes quitar si quieres, la imagen tienes que añadirla asi: <img src="/images/logo.png" alt="LO QUE TE DE LA REAL GANA">-->
 
             <div class="mt-10 flex items-center justify-center gap-x-6">
                 <a href="#" class="boton-inicio rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Ir a inicio</a>
