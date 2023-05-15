549c97c0a5e6c931a8c90136590fb99811f54279
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-15 23:40:10 +0200

    Mostrando más datos de obra en Obra.vue. Creado método calcularMediaEvaluaciones() en ObtenerObraController.php para pasar la media de evaluaciones como prop

--- app/Http/Controllers/ObtenerObraController.php
+++ app/Http/Controllers/ObtenerObraController.php
@@ -10,15 +10,33 @@ use Inertia\Inertia;
 class ObtenerObraController extends Controller
 {
     /**
+     * Para devolver la nota media de todas las evaluaciones
+     * @param $evaluaciones
+     * @return float
+     */
+    public function calcularMediaEvaluaciones($evaluaciones): float
+    {
+        $sumatorio = 0;
+        foreach ($evaluaciones as $eva){
+            $sumatorio += $eva['evaluacion'];
+        }
+        return round($sumatorio/count($evaluaciones), 1);
+    }
+
+    /**
      * Para obtener la información de una obra
      * @throws Exception
      */
     public function fichaPelicula($titulo){
         $titulo = rawurldecode($titulo);
+        $obra = Obra::with(['poster', 'secuelas:saga_id,obra_id,orden', 'criticas', 'directors:nombre,edad,defuncion,pais', 'festivals:obra_id,nombre,edicion', 'profesionals:obra_id,medio_id,autor,web,contenido', 'evaluaciones:obra_id,user_id,evaluacion', 'actors:nombre,nombre_real,edad,defuncion,pais', 'generos:genero'])->where('titulo', '=', "$titulo")->get();
+        $media = $this->calcularMediaEvaluaciones($obra[0]['evaluaciones']);
 
         return Inertia::render('Obra', [
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
-            'obra' => Obra::with(['poster', 'secuelas:saga_id,obra_id,orden', 'criticas', 'evaluaciones:obra_id,user_id,evaluacion', 'directors:nombre,edad,defuncion,pais', 'festivals:obra_id,nombre,edicion', 'profesionals:obra_id,medio_id,autor,web,contenido', 'actors:nombre,nombre_real,edad,defuncion,pais', 'generos:genero'])->where('titulo', '=', "$titulo")->get()]);
+            'obra' => $obra,
+            'mediaEvaluaciones' => $media,
+            ]);
     }
 }
--- resources/js/Pages/Obra.vue
+++ resources/js/Pages/Obra.vue
@@ -11,9 +11,11 @@ import es from "dayjs/locale/es";
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { Head } from "@inertiajs/vue3";
 
-defineProps(['obra']);
+defineProps(['obra', 'mediaEvaluaciones']);
 dayjs.extend(relativeTime);
 dayjs.locale('es');
+
+const media = 0;
 </script>
 
 <template>
@@ -21,38 +23,57 @@ dayjs.locale('es');
         <title>{{ obra[0].titulo }}</title>
         <meta name="description" content="Página de bienvenida">
     </Head>
-    <div class="w-[100vw] flex flex-col items-center [&>span]:font-bold">
-        <img class="w-[400px]" :src="'../posters/' + $page['props']['obra'][0]['poster']['ruta']" :alt="obra[0].poster.alt">
-        <p>
-            {{ $page['props']['obra'][0]['titulo'] }}
+    <div class="w-[66vw] flex flex-col items-center [&>span]:font-bold m-auto">
+        <img class="w-[400px]" :src="'../posters/' + obra[0]['poster']['ruta']" :alt="obra[0].poster.alt">
+        <p class="underline">
+            {{ obra[0]['titulo'] }} ({{obra[0]['titulo_original']}})-{{ obra[0]['fecha'] }}
         </p>
-        <span class="" v-show="$page['props']['obra'][0]['directors'][0]">Dirección:</span>
-        <p v-for="dir in $page['props']['obra'][0]['directors']">
+        <p  class="block" >{{ obra[0]['sinopsis'] }}</p>
+        <span v-show="obra[0]['directors'][0]">Dirección:</span>
+        <p v-for="dir in obra[0]['directors']">
             {{ dir['nombre'] }}
         </p>
-        <span v-show="$page['props']['obra'][0]['actors'][0]">Reparto:</span>
-        <p v-for="act in $page['props']['obra'][0]['actors']">
+        <span>País:</span>
+        <p>
+            {{ obra[0]['pais'] }}
+        </p>
+        <span>Productora:</span>
+        <p>
+            {{ obra[0]['productora'] }}
+        </p>
+        <span>Duracion:</span>
+        <p>
+            {{ parseInt(obra[0]['duracion']/60) }}h {{ parseInt(obra[0]['duracion']%60) }}min
+        </p>
+        <span v-show="obra[0]['actors'][0]">Reparto:</span>
+        <p v-for="act in obra[0]['actors']">
             {{ act['nombre'] }}
         </p>
-        <span v-show="$page['props']['obra'][0]['festivals'][0]">Premios: </span>
-        <p v-for="fest in $page['props']['obra'][0]['festivals']">
+        <span v-show="obra[0]['festivals'][0]">Premios: </span>
+        <p v-for="fest in obra[0]['festivals']">
             {{ fest['nombre'] }}({{ fest['edicion'] }})
         </p>
-        <span v-show="$page['props']['obra'][0]['generos'][0]">Generos: </span>
+        <span v-show="obra[0]['generos'][0]">Generos: </span>
         <ul>
-            <li v-for="gen in $page['props']['obra'][0]['generos']">
+            <li v-for="gen in obra[0]['generos']">
                 {{ gen['genero'] }}
             </li>
         </ul>
-        <span v-show="$page['props']['obra'][0]['criticas'][0]">Criticas: </span>
+        <span v-show="mediaEvaluaciones">Evaluaciones: </span>
         <ul>
-            <li v-for="cri in $page['props']['obra'][0]['criticas']">
+            <li>
+                {{ parseFloat(mediaEvaluaciones) }}
+            </li>
+        </ul>
+        <span v-show="obra[0]['criticas'][0]">Criticas: </span>
+        <ul>
+            <li v-for="cri in obra[0]['criticas']">
                 {{ dayjs(cri['modificada']).fromNow() }}-{{ cri['critica'] }}
             </li>
         </ul>
-        <span v-show="$page['props']['obra'][0]['profesionals'][0]">Criticas profesionales: </span>
+        <span v-show="obra[0]['profesionals'][0]">Criticas profesionales: </span>
         <ul>
-            <li v-for="pro in $page['props']['obra'][0]['profesionals']">
+            <li v-for="pro in obra[0]['profesionals']">
                 {{ pro['contenido'] }}
             </li>
         </ul>
