5435ec1bf02e9ff5004152c2849ddcedff90534f
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-15 13:33:14 +0200

    Refactorizados estilos en Poster.vue

--- resources/js/Components/Poster.vue
+++ resources/js/Components/Poster.vue
@@ -5,7 +5,7 @@ defineProps(['obra']);
 </script>
 
 <template>
-    <div class="w-full p-6">
+    <div class="w-[80vw] sm:w-full p-6">
         <Link :href="route('obra', encodeURIComponent(obra['titulo']))">
             <div class="cursor-pointer flex justify-center">
                 <h3 class="hover:text-white hover:bg-flamingo hover:overflow-visible hover:z-10 py-2 hover:py-2.5 px-3 hover:px-3 hover:text-xl hover:relative text-flamingo text-2xl font-bold underline font-oswald overflow-ellipsis overflow-hidden whitespace-nowrap">{{ obra['titulo'] }}</h3>
