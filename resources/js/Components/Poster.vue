734a32642bc6797c94627a6ec9f0225170cacbd6
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-15 12:41:43 +0200

    Refactorizados estilos en Obra.vue -> tailwind

--- resources/js/Components/Poster.vue
+++ resources/js/Components/Poster.vue
@@ -7,61 +7,14 @@ defineProps(['obra']);
 <template>
     <div class="w-full p-6">
         <Link :href="route('obra', encodeURIComponent(obra['titulo']))">
-            <div id="idTooltip" class="flex justify-center">
-                <h3>{{ obra['titulo'] }}</h3>
-                <h3 id='idTooltipText'>{{ obra['titulo'] }}</h3>
+            <div class="cursor-pointer flex justify-center">
+                <h3 class="hover:text-white hover:bg-flamingo hover:overflow-visible hover:z-10 py-2 hover:py-2.5 px-3 hover:px-3 hover:text-xl hover:relative text-flamingo text-2xl font-bold underline font-oswald overflow-ellipsis overflow-hidden whitespace-nowrap">{{ obra['titulo'] }}</h3>
+                <h3 class=" absolute text-2xl font-bold underline font-oswald  whitespace-nowrap top-0 py-[8px] px-[8px] bg-flamingo text-white group-hidden opacity-0 transition-opacity duration-500 ease-out" >{{ obra['titulo'] }}</h3>
             </div>
         </Link>
         <Link :href="route('obra', encodeURIComponent(obra['titulo']))">
-            <img id="idImgObra" class="w-full mt-3" :src="'posters/' + obra['poster']['ruta']"
+            <img class="hover:border-[10px] hover:border-flamingo hover:border-solid w-full mt-0" :src="'posters/' + obra['poster']['ruta']"
                  :alt="obra['poster']['alt']">
         </Link>
     </div>
 </template>
-
-<style scoped>
-/* TODO posibilidad de limpiar estilos en la medida de lo posible asignandolos a clases de tailwind */
-#idTooltip {
-    position: relative;
-    cursor: pointer;
-}
-
-#idTooltipText {
-    position: absolute;
-    top: 0;
-    padding: 8px 8px;
-    background: #e37f81;
-    color: white;
-    visibility: hidden;
-    opacity: 0;
-    transition: opacity 0.5s ease-out;
-}
-
-#idTooltip:hover #idTooltipText {
-    visibility: visible;
-    opacity: 1;
-    z-index: 10;
-    font-size: 1.1rem;
-}
-
-#idTooltip:hover h3 {
-    visibility: hidden;
-}
-
-#idImgObra:hover {
-    border: 10px #e37f81 solid;
-}
-
-h3 {
-    position: relative;
-    color: #e37f81;
-    font-size: 1.6rem;
-    font-weight: bold;
-    text-decoration: underline;
-    font-family: 'Oswald', sans-serif;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    white-space: nowrap;
-}
-
-</style>
