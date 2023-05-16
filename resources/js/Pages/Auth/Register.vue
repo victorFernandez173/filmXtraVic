395d2e9d8a29b20355f60d7402209a3558a22d08
elena <elena.gatajimenez@gmail.com>
2023-05-16 12:05:58 +0200

    Fecha de nacimiento en register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -127,6 +127,10 @@ const submit = () => {
                             <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900">Confirmar contraseña</label>
                             <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-flamingo focus:border-flamingo block w-full p-2.5" required="">
                         </div>
+                        <div>
+                            <label for="birth" class="block mb-2 text-sm font-medium text-gray-900">Fecha de nacimiento</label>
+                            <input type="birth" name="birth" id="birth" placeholder="01/01/2001" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-flamingo focus:border-flamingo block w-full p-2.5" required="">
+                        </div>
                         <button type="submit" class="w-full text-white bg-flamingo hover:bg-black hover:text-flamingo focus:bg-flamingo focus:ring-flamingo focus:border-flamingo focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Crear cuenta</button>
                         <p class="text-sm font-light text-gray-500">
                             ¿Ya tienes cuenta? <a href="#" class="font-medium text-primary-600 hover:underline">¿Inicia sesión aquí!</a>
