3c611340abc956dee4f5e1e27bcd8551124a090a
elena <elena.gatajimenez@gmail.com>
2023-05-16 11:22:33 +0200

    Finalizada pagina de login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -28,7 +28,7 @@ const submit = () => {
 };
 </script>
 
-<template>
+<!--<template>
     <Head>
         <title>Login</title>
         <meta name="description" content="Página de logueo">
@@ -91,4 +91,49 @@ const submit = () => {
             </PrimaryButton>
         </div>
     </form>
+</template>-->
+
+<template>
+    <section class="bg-cover bg-[url('images/posters-fondo.png')]">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                <img class="w-60 mr-2" src="/images/logo-blanco.png" alt="Logo FlimXtra">
+            </a>
+            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Inicia sesión con tu cuenta
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Correo electrónico</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-flamingo focus:border-flamingo block w-full p-2.5" placeholder="nombre@correo.com" required="">
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Contraseña</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-flamingo focus:border-flamingo block w-full p-2.5" required="">
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-start">
+                                <div class="flex items-center h-5">
+                                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-flamingo focus:border-flamingo" required="">
+                                </div>
+                                <div class="ml-3 text-sm">
+                                    <label for="remember" class="text-gray-500">Recuérdame</label>
+                                </div>
+                            </div>
+                            <a href="#" class="text-sm font-medium text-primary-600 hover:underline">¿Has olvidado tu contraseña?</a>
+                        </div>
+                        <button type="submit" class="w-full text-white bg-flamingo hover:bg-black hover:text-flamingo focus:bg-flamingo focus:ring-flamingo focus:border-flamingo focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                        <p class="text-sm font-light text-gray-500">
+                            ¿Aún no tienes cuenta? <a href="#" class="font-medium text-primary-600 hover:underline">¡Regístrate aquí!</a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
+
+<style>
+</style>
