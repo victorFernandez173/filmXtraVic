e5df582ea19d76848b275144e4c7f47b0f026e3e
elena <elena.gatajimenez@gmail.com>
2023-05-16 16:52:44 +0200

    Página de obra

--- resources/js/Pages/Obra2.vue
+++ resources/js/Pages/Obra2.vue
@@ -85,10 +85,10 @@ const media = 0;
     <div class="container mx-auto mt-10 mb-10">
         <h1 class="text-center font-bold text-flamingo underline text-3xl">Harry Potter y el misterio del príncipe</h1>
         <!--3 apartados para poster, datos y valoraciones-->
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mt-10">
             <!--Poster-->
-            <div class="justify-center m-auto">
-                <img src="../../../public/posters/Harry_Potter_and_the_Half-Blood_Prince_(Harry_Potter_6).png" alt="Poster">
+            <div class="justify-center m-auto w-9/12">
+                <img class="w-80" src="../../../public/posters/Harry_Potter_and_the_Half-Blood_Prince_(Harry_Potter_6).png" alt="Poster">
                 <!--Puntuacion-->
                 <div class="flex justify-center">
                     <div class="flex items-center mt-2 text-yellow-300 m-auto">
@@ -103,7 +103,7 @@ const media = 0;
             </div>
 
             <!--Datos pelicula-->
-            <div class="flex justify-center col-span-2 ml-10 mr-10">
+            <div class="flex justify-center ml-10 mr-10 w-1/4">
                 <ul>
                     <!--Datos de la pelicula-->
                     <li class="list-disc font-bold underline text-flamingo text-xl">Obra:</li>
@@ -128,31 +128,29 @@ const media = 0;
                     </ul>
                 </ul>
             </div>
+        </div>
 
-            <!--Críticas-->
-            <div class="pl-10 justify-center bg-flamingo text-white rounded">
-                <li class="list-disc font-bold underline text-black text-xl mt-5">Críticas profesionales:</li>
-                <ul>
-                    <li class="list-disc ml-5">Merece la pena</li>
-                </ul>
-                <li class="list-disc font-bold underline text-black text-xl mt-5">Críticas de nuestros usuarios:</li>
-                <ul>
-                    <li class="list-disc ml-5">Es mi película favorita</li>
-                    <li class="list-disc ml-5">Un rollo no la recomiendo</li>
-                    <li class="list-disc ml-5">Para pasar la tarde</li>
-                    <li class="list-disc ml-5">Entretenida </li>
-                    <!--Mano abajo-->
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                        <path d="M18.905 12.75a1.25 1.25 0 01-2.5 0v-7.5a1.25 1.25 0 112.5 0v7.5zM8.905 17v1.3c0 .268-.14.526-.395.607A2 2 0 015.905 17c0-.995.182-1.948.514-2.826.204-.54-.166-1.174-.744-1.174h-2.52c-1.242 0-2.26-1.01-2.146-2.247.193-2.08.652-4.082 1.341-5.974C2.752 3.678 3.833 3 5.005 3h3.192a3 3 0 011.342.317l2.733 1.366A3 3 0 0013.613 5h1.292v7h-.963c-.684 0-1.258.482-1.612 1.068a4.012 4.012 0 01-2.165 1.73c-.433.143-.854.386-1.012.814-.16.432-.248.9-.248 1.388z" />
-                    </svg>
-                    <!--Mano arriba-->
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                        <path d="M1 8.25a1.25 1.25 0 112.5 0v7.5a1.25 1.25 0 11-2.5 0v-7.5zM11 3V1.7c0-.268.14-.526.395-.607A2 2 0 0114 3c0 .995-.182 1.948-.514 2.826-.204.54.166 1.174.744 1.174h2.52c1.243 0 2.261 1.01 2.146 2.247a23.864 23.864 0 01-1.341 5.974C17.153 16.323 16.072 17 14.9 17h-3.192a3 3 0 01-1.341-.317l-2.734-1.366A3 3 0 006.292 15H5V8h.963c.685 0 1.258-.483 1.612-1.068a4.011 4.011 0 012.166-1.73c.432-.143.853-.386 1.011-.814.16-.432.248-.9.248-1.388z" />
-                    </svg>
-
-
-                </ul>
-            </div>
+        <!--Críticas-->
+        <div class="grid grid-cols-1 pl-10 justify-center bg-flamingo text-white rounded">
+            <li class="list-disc font-bold underline text-black text-xl mt-5">Críticas profesionales:</li>
+            <ul>
+                <li class="list-disc ml-5">Merece la pena</li>
+            </ul>
+            <li class="list-disc font-bold underline text-black text-xl mt-5">Críticas de nuestros usuarios:</li>
+            <ul>
+                <li class="list-disc ml-5">Es mi película favorita</li>
+                <li class="list-disc ml-5">Un rollo no la recomiendo</li>
+                <li class="list-disc ml-5">Para pasar la tarde</li>
+                <li class="list-disc ml-5">Entretenida </li>
+                <!--Mano abajo-->
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <path d="M18.905 12.75a1.25 1.25 0 01-2.5 0v-7.5a1.25 1.25 0 112.5 0v7.5zM8.905 17v1.3c0 .268-.14.526-.395.607A2 2 0 015.905 17c0-.995.182-1.948.514-2.826.204-.54-.166-1.174-.744-1.174h-2.52c-1.242 0-2.26-1.01-2.146-2.247.193-2.08.652-4.082 1.341-5.974C2.752 3.678 3.833 3 5.005 3h3.192a3 3 0 011.342.317l2.733 1.366A3 3 0 0013.613 5h1.292v7h-.963c-.684 0-1.258.482-1.612 1.068a4.012 4.012 0 01-2.165 1.73c-.433.143-.854.386-1.012.814-.16.432-.248.9-.248 1.388z" />
+                </svg>
+                <!--Mano arriba-->
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <path d="M1 8.25a1.25 1.25 0 112.5 0v7.5a1.25 1.25 0 11-2.5 0v-7.5zM11 3V1.7c0-.268.14-.526.395-.607A2 2 0 0114 3c0 .995-.182 1.948-.514 2.826-.204.54.166 1.174.744 1.174h2.52c1.243 0 2.261 1.01 2.146 2.247a23.864 23.864 0 01-1.341 5.974C17.153 16.323 16.072 17 14.9 17h-3.192a3 3 0 01-1.341-.317l-2.734-1.366A3 3 0 006.292 15H5V8h.963c.685 0 1.258-.483 1.612-1.068a4.011 4.011 0 012.166-1.73c.432-.143.853-.386 1.011-.814.16-.432.248-.9.248-1.388z" />
+                </svg>
+            </ul>
         </div>
 
         <!--1 apartado para el trailer-->
