57717a0b7e7c1a17ace1c0d35fa0117ffaae2ef5
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-16 17:26:26 +0200

    En ObtenerObraController.php ahora se hace la consulta principal en un método para hacer más liviano el método que renderiza la vista. Iniciado método para obtener secuela y precuela si las hay, funcionando (devuelve los respectivos id si los hay)

--- app/Http/Controllers/ObtenerObraController.php
+++ app/Http/Controllers/ObtenerObraController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Obra;
 use DB;
 use Exception;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use LaravelIdea\Helper\App\Models\_IH_Obra_C;
+use LaravelIdea\Helper\App\Models\_IH_Obra_QB;
 
 class ObtenerObraController extends Controller
 {
     /**
-     * Para devolver la nota media de todas las evaluaciones
+     * Para devolver la nota media de todas las evaluaciones, devuelve 0 si no hay evaluaciones
      * @param $evaluaciones
      * @return float
      */
@@ -58,27 +62,51 @@ class ObtenerObraController extends Controller
         return $saga;
     }
 
-     public function obtenerSecuelaPrecuela($secuelas){
-        if(isset($secuelas['saga_id'])){
+    /**
+     * Para obtener la secuela/precuela si las hubiera
+     * @param $obra
+     * @return array|null
+     */
+     public function obtenerSecuelaPrecuela($obra){
+         // Si es una secuela esta obra
+        if(isset($obra[0]['secuelas'])){
+            // Obtenemos su orden en la saga
+            $orden = $obra[0]['secuelas']['orden'];
             $secuelaPrecuela = array();
-            $secuelas = DB::table('secuelas')->select('saga_id', 'obra_id', 'orden')->where('saga_id', '=', $secuelas['saga_id'])->get();
-            ddd($secuelas);
+            // Obtenemos las array con las pelis de la saga
+            $secuelas = DB::table('secuelas')->select('saga_id', 'obra_id', 'orden')->where('saga_id', '=', $obra[0]['secuelas']['saga_id'])->get();
+            //proceso el array $secuelas para orden + 1 (secuela) y orden - 1 (precuela)
+            foreach ($secuelas as $secuela){
+                if(($orden - 1)  == $secuela->orden || ($orden + 1) == $secuela->orden ){
+                    array_push($secuelaPrecuela, $secuela->obra_id);
+                }
+            }
             return $secuelaPrecuela;
         }
         return null;
     }
 
+    /**
+     * Para obtener los datos iniciales de la obra con relaciones
+     * @param $titulo
+     * @return Obra[]|Builder[]|\Illuminate\Database\Eloquent\Collection|_IH_Obra_C|_IH_Obra_QB[]
+     */
+    public function obtenerDatosObra($titulo){
+        return Obra::with(['poster', 'secuelas:saga_id,obra_id,orden', 'criticas', 'directors:nombre,edad,defuncion,pais', 'festivals:obra_id,nombre,edicion', 'profesionals:obra_id,medio_id,autor,contenido', 'evaluaciones:obra_id,user_id,evaluacion', 'actors:nombre,nombre_real,edad,defuncion,pais', 'generos:genero'])->where('titulo', '=', "$titulo")->get();
+    }
+
 
     /**
-     * Para obtener la información de una obra
+     * Se organizan y recopilan todos los datos necesarios para pasar a la ficha de película
      * @throws Exception
      */
     public function fichaPelicula($titulo){
         $titulo = rawurldecode($titulo);
-        $obra = Obra::with(['poster', 'secuelas:saga_id,obra_id,orden', 'criticas', 'directors:nombre,edad,defuncion,pais', 'festivals:obra_id,nombre,edicion', 'profesionals:obra_id,medio_id,autor,contenido', 'evaluaciones:obra_id,user_id,evaluacion', 'actors:nombre,nombre_real,edad,defuncion,pais', 'generos:genero'])->where('titulo', '=', "$titulo")->get();
+        $obra = $this->obtenerDatosObra($titulo);
         $media = $this->calcularMediaEvaluaciones($obra[0]['evaluaciones']);
         $criticasLikes = $this->obtenerArrayInfoCriticas($obra[0]['criticas']);
         $saga = $this->obtenerSaga($obra[0]['secuelas']);
+        $secuelaPrecuela = $this->obtenerSecuelaPrecuela($obra);
 
         return Inertia::render('Obra', [
             'canLogin' => Route::has('login'),
@@ -87,6 +115,7 @@ class ObtenerObraController extends Controller
             'mediaEvaluaciones' => $media,
             'criticas' => $criticasLikes,
             'saga' => $saga,
+            'secuelaPrecuela' => $secuelaPrecuela,
             ]);
     }
 }
