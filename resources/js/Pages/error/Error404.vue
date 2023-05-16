cea5ea44e11864e70c1267f24cce6fb8429ccae1
elena <elena.gatajimenez@gmail.com>
2023-05-16 09:16:10 +0200

    Cambio de color de las letras de error 404 y finalización página error 404

--- resources/js/Pages/error/Error404.vue
+++ resources/js/Pages/error/Error404.vue
@@ -12,9 +12,11 @@ defineProps([ 'status', 'message'])
     <div class="grid h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 bg-cover bg-[url('gif/error-fondo.gif')]">
         <div class="text-center">
             <!--<p class="texto-404 text-base font-semibold">Error {{ status }}</p>-->
-            <!--<h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{{ message }}</h1>-->
             <img src="/images/logo-error.png" class="m-auto w-60 mb-10" alt="Logo Error 404">
-            <p class="mt-4 text-2xl leading-7 text-gray-800">Pero si vuelves al inicio, en FilmXtra encontrarás todo lo que necesitas.</p>
+            <div class="color-fondo-letras rounded">
+                <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{{ message }}</h1>
+                <p class="mt-4 text-2xl leading-7 text-gray-800">Pero si vuelves al inicio, en FilmXtra encontrarás todo lo que necesitas.</p>
+            </div>
             <img src="/gif/error-gif.gif" class="m-auto mt-7 w-80" alt="Gif error cine">
             <div class="mt-10 flex items-center justify-center gap-x-6">
                 <Link :href="route('/')" class="hover:text-flamingo hover:bg-black bg-flamingo rounded-md px-3.5 py-2.5 text-sm font-semibold text-white ">Ir a inicio</Link>
@@ -24,3 +26,9 @@ defineProps([ 'status', 'message'])
         </div>
     </div>
 </template>
+
+<style>
+.color-fondo-letras {
+    background-color: rgba(255, 255, 255, 0.4);
+}
+</style>
