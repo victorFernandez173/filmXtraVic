e38305ce00574c83fb4cb301881b2de1850a0b21
elena <elena.gatajimenez@gmail.com>
2023-05-18 20:50:51 +0200

    Página confirm password

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -3,7 +3,7 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import {Head, useForm} from '@inertiajs/vue3';
+import {Head, Link, useForm} from '@inertiajs/vue3';
 
 const form = useForm({
     password: '',
@@ -16,7 +16,7 @@ const submit = () => {
 };
 </script>
 
-<template>
+<!--<template>
     <Head>
         <title>Confirmar password</title>
         <meta name="description" content="Pagina de confirmacion de password">
@@ -47,4 +47,41 @@ const submit = () => {
             </PrimaryButton>
         </div>
     </form>
+</template>-->
+
+<template>
+    <section class="bg-cover bg-[url('images/posters-fondo.png')]">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <Link :href="route('/')" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                <img class="w-60 mr-2" src="/images/logo-blanco.png" alt="Logo FlimXtra">
+            </Link>
+            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Confirmación de la contraseña
+                    </h1>
+                    <h3 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Esta área es segura. Por favor, confirme su contraseña antes de continuar.
+                    </h3>
+                    <form class="space-y-4 md:space-y-6" @submit.prevent="submit">
+                        <div>
+                            <InputLabel for="password" value="Contraseña" class="block mb-2 text-sm font-medium text-gray-900" />
+                            <TextInput
+                                id="password"
+                                type="password"
+                                placeholder="••••••••"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-flamingo focus:border-flamingo block w-full p-2.5"
+                                v-model="form.password"
+                                autocomplete="current-password"
+                            />
+                            <InputError class="mt-2" :message="form.errors.password"/>
+                        </div>
+                        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Confirmar
+                        </PrimaryButton>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -81,8 +81,7 @@ const verificationLinkSent = computed(() => props.status === 'verification-link-
                                 as="button"
                                 class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >Cerrar sesión
-                            </Link
-                            >
+                            </Link>
                         </div>
                     </form>
                 </div>
