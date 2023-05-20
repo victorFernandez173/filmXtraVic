bcae0ca69c8db5a117837591d1fedc3090edec56
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-20 13:25:52 +0200

    Implementación finalizada de VerifyEmail.vue

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -18,39 +18,6 @@ const submit = () => {
 const verificationLinkSent = computed(() => props.status === 'verification-link-sent');
 </script>
 
-<!--<template>
-    <Head>
-        <title>Verificación de email</title>
-        <meta name="description" content="Página de verificación de email">
-    </Head>
-
-    <div class="mb-4 text-sm text-gray-600">
-        Thanks for signing up! Before getting started, could you verify your email address by clicking on the link
-        we just emailed to you? If you didn't receive the email, we will gladly send you another.
-    </div>
-
-    <div class="mb-4 font-medium text-sm text-green-600" v-if="verificationLinkSent">
-        A new verification link has been sent to the email address you provided during registration.
-    </div>
-
-    <form @submit.prevent="submit">
-        <div class="mt-4 flex items-center justify-between">
-            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Resend Verification Email
-            </PrimaryButton>
-
-            <Link
-                :href="route('logout')"
-                method="post"
-                as="button"
-                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >Log Out
-            </Link
-            >
-        </div>
-    </form>
-</template>-->
-
 <template>
     <Head>
         <title>Verificación de email</title>
@@ -68,6 +35,9 @@ const verificationLinkSent = computed(() => props.status === 'verification-link-
                     </h1>
                     <h3 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                         ¡Gracias por registrarte en FilmXtra! Revisa tu correo y acepta nuestras condiciones para verificar tu cuenta. Si no recibiste ningún correo, haz click en el botón de abajo y te enviaremos otro.
+                    </h3>
+                    <h3 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-xl" v-if="verificationLinkSent">
+                        A new verification link has been sent to the email address you provided during registration.
                     </h3>
                     <form @submit.prevent="submit">
                         <div class="mt-4 flex items-center justify-between">
