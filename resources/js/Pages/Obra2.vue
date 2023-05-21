d3c1c4b21e2fb5463280fc21a75522648028fd7a
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-21 13:52:30 +0200

    Añadidas criticas profesionales a Obra.vue

--- resources/js/Pages/Obra2.vue
+++ resources/js/Pages/Obra2.vue
@@ -5,11 +5,15 @@
                 {{ dayjs(cri['fecha']).fromNow() }}-{{ cri['critica'] }}(Likes:{{ cri['likes'] }})[{{cri['usuario'][0]['name']}}]
             </li>
         </ul>
+
+
+
+
         <span v-if="profesionales">Criticas profesionales: </span>
         <ul>
             <li v-for="pro in profesionales">
                 {{ pro['contenido'] }}({{ pro['autor'] }})[{{ pro['medio'] }}-
-                <a class="hover:text-red-600 text-flamingo underline" :href="pro['web']" target="_blank">{{ pro['web'] }}</a>]
+                <a class="hover:text-red-600 text-flamingo underline" >{{ pro['web'] }}</a>]
             </li>
         </ul>
  -->
