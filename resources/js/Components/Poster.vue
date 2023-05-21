59ee524ef2c19fe48119fb3c6de9b686848379a7
victorFernandez173 <victor.fer.gor@proton.me>
2023-05-21 22:07:33 +0200

    Correcciones en inserts de profesionals

--- resources/js/Components/Poster.vue
+++ resources/js/Components/Poster.vue
@@ -10,7 +10,7 @@ defineProps(['obra', 'titulo']);
         <Link :href="route('obra', encodeURIComponent(obra['titulo']))">
             <div class="cursor-pointer flex justify-center h-[50px] content-center -mb-[10px]">
                 <h3 :class="titulo" class="hover:text-white hover:bg-flamingo hover:overflow-visible hover:z-10 py-2 hover:py-2.5 px-3 hover:px-3 hover:relative text-flamingo font-bold underline font-oswald overflow-ellipsis overflow-hidden whitespace-nowrap">{{ obra['titulo'] }}</h3>
-                <h3 :class="titulo" class=" absolute font-bold underline font-oswald  whitespace-nowrap top-0 py-[8px] px-[8px] bg-flamingo text-white group-hidden opacity-0 transition-opacity duration-500 ease-out" >{{ obra['titulo'] }}</h3>
+                <h3 :class="titulo" class="absolute font-bold underline font-oswald  whitespace-nowrap top-0 py-[8px] px-[8px] bg-flamingo text-white group-hidden opacity-0 transition-opacity duration-500 ease-out" >{{ obra['titulo'] }}</h3>
             </div>
         </Link>
         <Link :href="route('obra', encodeURIComponent(obra['titulo']))">
