57c9e530b02b58550a957e3a23e1b1b3fa4c4e3d
elena <elena.gatajimenez@gmail.com>
2023-05-24 19:33:10 +0200

    Pequeño cambio en posters

--- resources/js/Components/Trailers.vue
+++ resources/js/Components/Trailers.vue
@@ -7,7 +7,7 @@ defineProps(['obra']);
         <!--Trailer-->
         <div v-if="obra[0]['trailer']" class="flex justify-center flex-wrap">
             <!-- Titulo de la seccion -->
-            <h3 class="w-full text-center text-flamingo font-bold underline text-2xl pb-3">Trailers de {{ obra[0]['titulo'] }}</h3>
+            <h3 class="w-full text-center text-flamingo font-bold underline text-2xl pb-3">Trailer de {{ obra[0]['titulo'] }}</h3>
             <iframe width="1300" height="600" :src="obra[0]['trailer']['trailers']"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
