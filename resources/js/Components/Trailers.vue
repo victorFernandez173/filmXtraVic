5f7b198bee5708867b4193ec42e1b4f74d9f8545
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-24 14:50:55 +0200

    Corregido fallo en la comprobacion de la existencia de trailers en Trailers.vue....

--- resources/js/Components/Trailers.vue
+++ resources/js/Components/Trailers.vue
@@ -5,7 +5,7 @@ defineProps(['obra']);
 <template>
     <div class="grid grid-cols-1 pt-10">
         <!--Trailer-->
-        <div v-if="obra[0]['trailer']['trailers']" class="flex justify-center flex-wrap">
+        <div v-if="obra[0]['trailer']" class="flex justify-center flex-wrap">
             <!-- Titulo de la seccion -->
             <h3 class="w-full">Trailers de {{ obra[0]['titulo'] }}</h3>
             <iframe width="1300" height="600" :src="obra[0]['trailer']['trailers']"
