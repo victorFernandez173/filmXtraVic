3432494dd21f9b2f7f63668ae427f338b3e2d0ba
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-24 12:36:09 +0200

    Pasando numero valoraciones a top y recolocado abajo en el diseño

--- resources/js/Components/Poster.vue
+++ resources/js/Components/Poster.vue
@@ -7,7 +7,6 @@ defineProps(['obra', 'titulo']);
 
 <template>
     <div class="w-[100%] sm:w-full p-6">
-        <h3 v-if="obra['nota_media']">{{ parseFloat(obra['nota_media']).toFixed(1) }} ({{obra['num_valoraciones']}} valoraciones)</h3>
         <Link :href="route('obra', encodeURIComponent(obra['titulo']))">
             <div class="cursor-pointer flex justify-center h-[50px] content-center -mb-[10px]">
                 <h3 :class="titulo" class="hover:text-white hover:bg-flamingo hover:overflow-visible hover:z-10 py-2 hover:py-2.5 px-3 hover:px-3 hover:relative text-flamingo font-bold underline font-oswald overflow-ellipsis overflow-hidden whitespace-nowrap">{{ obra['titulo'] }}</h3>
@@ -18,5 +17,8 @@ defineProps(['obra', 'titulo']);
             <img class="hover:border-[10px] border-[10px] hover:border-flamingo border-white hover:border-solid border-solid w-full mt-0" :src="'../posters/' + [obra['ruta'] ? obra['ruta'] : obra['poster']['ruta']]"
                  :alt="obra['alt']">
         </Link>
+        <h3 v-if="obra['nota_media']">{{ parseFloat(obra['nota_media']).toFixed(1) }}</h3>
+        <h3 v-if="obra['num_valoraciones']">({{obra['num_valoraciones']}} valoraciones)</h3>
+
     </div>
 </template>
