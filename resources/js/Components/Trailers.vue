21ce96536729261a301198546730283af3ab3a46
victorFernandez173 <victor.fer.gor@proton.me>
2023-05-31 00:02:41 +0200

    Corregidos problemas de estilo con el iframe del video del trailer

--- resources/js/Components/Trailers.vue
+++ resources/js/Components/Trailers.vue
@@ -8,7 +8,7 @@ defineProps(['obra']);
             <!-- Titulo de la seccion -->
             <h3 class="w-full text-center text-flamingo font-bold underline text-2xl">Trailer de {{ obra[0]['titulo'] }}</h3>
             <iframe
-                class="w-[100vw] h-[57vw]"
+                class="w-[100vw] h-auto aspect-video"
                 :src="obra[0]['trailer']['trailers']"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
