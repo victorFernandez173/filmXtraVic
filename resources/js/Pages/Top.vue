db23abf7a2faec3071018d879d3a09c2206fa217
victor.fernandez173 <victor.fer.gor@proton.me>
2023-05-31 17:14:40 +0200

    Añadido margen entre posters y formulario en top filmxtra

--- resources/js/Pages/Top.vue
+++ resources/js/Pages/Top.vue
@@ -50,7 +50,7 @@ defineProps(['obras', 'titulo', 'filtros']);
                 </p>
                 <div v-else class="w-full text-center"><p>Sin filtros</p></div>
                 <!-- Seccion Principal de contenido -->
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 seccion-peliculas text-center w-full justify-items-center pl-8 pr-8">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 seccion-peliculas text-center w-full justify-items-center pl-8 pr-8 mt-8">
                     <!-- Cada poster es un componente -->
                     <Poster v-for="obra in obras.data" :key="obra['id']" :obra="obra" :titulo="`text-2xl hover:text-xl`" :info="true"/>
                 </div>
