262e5698762289039b3d620afa9ed303e38edf35
victorFernandez173 <victor.fer.gor@proton.me>
2023-06-01 20:44:36 +0200

    Margenes titulo trailer en ficha obra

--- resources/js/Components/Trailers.vue
+++ resources/js/Components/Trailers.vue
@@ -4,9 +4,9 @@ defineProps(['obra']);
 
 <template>
         <!--Trailer-->
-        <div v-if="obra[0]['trailer']" class="mt-8 flex justify-center flex-wrap ">
+        <div v-if="obra[0]['trailer']" class="mt-16 flex justify-center flex-wrap ">
             <!-- Titulo de la seccion -->
-            <h3 class="w-full text-center text-flamingo font-bold underline text-2xl">Trailer de {{ obra[0]['titulo'] }}</h3>
+            <h3 class="w-full text-center text-flamingo font-bold underline text-2xl mb-4">Trailer de {{ obra[0]['titulo'] }}</h3>
             <iframe
                 class="w-[100vw] h-auto aspect-video"
                 :src="obra[0]['trailer']['trailers']"
