451fd4fdb81825ab537e22cb97bd0919a656cb62
victor.fernandez173 <victor.fer.gor@proton.me>
2023-06-08 20:36:04 +0200

    socialite google fix vic laptop

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\EmailController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\RedirectResponse;
 use Notification;
@@ -19,25 +21,27 @@ class SocialAuthController extends Controller
      * Redirect the user to the Google authentication page
      *
      */
-    public function redirectToProvider(): RedirectResponse
+    public function redirectToProvider()
     {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')->with(["prompt" => "select_account"])->redirect();
     }
 
     public function handleCallback(): RedirectResponse
     {
         try{
             $user = Socialite::driver('google')->user();
+            $userExist = User::where('google_id', $user->id)->first();
         } catch(\Exception $e) {
             return redirect('/login');
         }
 
-        $userExist = User::where('google_id', $user->id)->first();
 
-        if ($userExist !== null) {
+
+        if ($userExist) {
             Auth::login($userExist);
-        }else{
+        } else {
             $newUser = User::create([
+                'name' => $user->name,
                 'email' => $user->email,
                 'google_id' => $user->id,
                 'password' => Hash::make($user->id),
@@ -47,6 +51,7 @@ class SocialAuthController extends Controller
 
             Auth::login($newUser);
         }
+        // TODO redigir a la url de origen, quizas con SESSION ya que previous no rula
         return redirect('/');
     }
 }
